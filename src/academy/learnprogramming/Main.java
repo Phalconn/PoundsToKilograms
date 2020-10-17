1dffcaa8c4bc165cf2f93803d864a1703ac90072
Abdoulie Njai <tyscenjai@hotmail.com>
2020-10-16 22:46:05 -0700

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+    double myKilogramsVariable = 0.45359237;
+    double myPoundVariable = 200;
+        System.out.println("AmountInKilograms =" + myKilogramsVariable * myPoundVariable);
+    }
+    
+
+    }
+}
